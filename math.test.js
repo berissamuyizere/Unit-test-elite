3b5c8d3ca173df35c69beb7d5ae9aee3390943fa
BerissaMuyizere <muyizereberissa@gmail.com>
2025-03-27 09:48:40 +0300

    unit test

--- math.test.js
+++ math.test.js
@@ -9,7 +9,3 @@ test('subtracts 5-2 to equal to 3', ()=>{
 });
 
 
- test('divides 10 / 2 to equal 5', ()=>{
-    expect(divide(10,2)).toBe(5);
-    expect(()=> divide(10,0)).toThrow('Cannot divide by zero')
- });
