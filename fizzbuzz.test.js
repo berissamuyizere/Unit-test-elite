f206736965067e271c7ee4c3dd0471d3e76c82e3
habtamneshmarye <habtamneshmarye@gmail.com>
2025-03-27 10:05:31 +0300

    fizzbuzz

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -3,7 +3,9 @@ test('returns "FizzBuzz" for 15', ()=>{
     expect(fizzBuzz(15)).toBe('FizzBuzz');
 });
 
-
+test('returns "Fizz" for 9', ()=>{
+    expect(fizzBuzz(9)).toBe('Fizz');
+});
 
 test('returns "Buzz" for 10', ()=>{
     expect(fizzBuzz(10)).toBe('Buzz');
