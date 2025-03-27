223118d699be56cbd3924f4ee239339b3a3fbb56
kezaqueen <rutagengwakezaqueengmail.com>
2025-03-27 09:40:34 +0300

    unit testing for different files

--- math.test.js
+++ math.test.js
@@ -8,9 +8,7 @@ test('subtracts 5-2 to equal to 3', ()=>{
     expect(subtract(5,2)).toBe(3);
 });
 
-test('multiplies 3 * 4 to equal to 12', ()=>{
-    expect(multiply(3,4)).toBe(12);
-});
+
  test('divides 10 / 2 to equal 5', ()=>{
     expect(divide(10,2)).toBe(5);
     expect(()=> divide(10,0)).toThrow('Cannot divide by zero')
