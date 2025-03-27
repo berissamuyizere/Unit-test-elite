0d56c0b573c0860fc53d1426e71ec35e69c28105
BerissaMuyizere <muyizereberissa@gmail.com>
2025-03-27 09:54:08 +0300

    testing even numbers

--- math.test.js
+++ math.test.js
@@ -8,7 +8,6 @@ test('subtracts 5-2 to equal to 3', ()=>{
     expect(subtract(5,2)).toBe(3);
 });
 
-
 test('divides 10 / 2 to equal 5', ()=>{
     expect(divide(10,2)).toBe(5);
     expect(()=> divide(10,0)).toThrow('Cannot divide by zero')
--- numbers.test.js
+++ numbers.test.js
@@ -4,6 +4,3 @@ test('4 is even', ()=>{
     expect(isEven(4)).toBe(true);
 });
 
-test('3 is not even',()=>{
-    expect(isEven(3)).toBe(false);
-});
