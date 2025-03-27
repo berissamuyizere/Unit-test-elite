6d8cbc002a34a8b2bc8dc0a4033922d63cebd686
kezaqueen <rutagengwakezaqueengmail.com>
2025-03-27 10:35:59 +0300

    unit testing for different files

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -9,3 +9,6 @@ test('returns "Fizz" for 9', ()=>{
 test('returns "Buzz" for 10', ()=>{
     expect(fizzBuzz(10)).toBe('Buzz');
 });
+test('returns "7" for 7',()=>{
+    expect(fizzBuzz(7)).toBe('7');
+});
